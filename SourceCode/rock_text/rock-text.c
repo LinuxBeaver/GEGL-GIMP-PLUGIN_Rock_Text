648b54a8731d69ef87a8ad7ceee2648c74b11d51
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2025-04-10 00:28:37 -0400

    Add files via upload

--- SourceCode/rock_text/rock-text.c
+++ SourceCode/rock_text/rock-text.c
@@ -53,6 +53,20 @@ enum_value   (ROCKTEXT_SHOW_LEGACY, "legacy", N_("Legacy Sliders for technical u
   enum_end (guiendrockfun)
 
 
+property_enum (rockblend, _("Blend mode of rock emboss"),
+    againnicherock, nicherock,
+    multiply)
+   description  (_("Blend mode of the rock's emboss"))
+
+
+enum_start (nicherock)
+  enum_value (multiply,      "multiply",
+              N_("Multiply"))
+  enum_value (hardlight,      "hardlight",
+              N_("Hard Light"))
+enum_end (againnicherock)
+
+
 property_int  (size, _("Internal Median Blur Radius"), 0)
   value_range (0, 10)
   ui_range    (0, 10)
@@ -110,14 +124,19 @@ property_int  (size2, _("Crumbled mini rocks effect "), 2)
 
 
 property_double (azimuth, _("Light Rotation"), 180.0)
-    description (_("Light angle (degrees)"))
+    description (_("Light angle (degrees) of the internal emboss"))
     value_range (0, 360)
     ui_meta ("unit", "degree")
     ui_meta ("direction", "ccw")
 
 property_double (elevation, _("Elevation"), 80.0)
-    description (_("Elevation angle (degrees)"))
+    description (_("Elevation angle (degrees) of the internal emboss"))
     value_range (0, 180)
+    ui_meta ("unit", "degree")
+
+property_int (depth, _("Depth"), 20)
+    description (_("Depth of the internal emboss"))
+    value_range (20, 100)
     ui_meta ("unit", "degree")
 
 property_double (grains, _("Grains over rock"), 0.8)
@@ -193,154 +212,244 @@ ui_meta ("visible", "guichange {legacy}")
 
 #include "gegl-op.h"
 
+typedef struct
+{
+  GeglNode *input;
+  GeglNode *output;
+  GeglNode *color;
+  GeglNode *median;
+  GeglNode *median2;
+  GeglNode *gaussian;
+  GeglNode *noise;
+  GeglNode *exposure;
+  GeglNode *alpha;
+  GeglNode *shift;
+  GeglNode *emboss;
+  GeglNode *normal;
+  GeglNode *graph;
+  GeglNode *opacity;
+  GeglNode *edgesmooth;
+  GeglNode *smooth;
+  GeglNode *multiply;
+  GeglNode *mcol;
+  GeglNode *hardlight;
+  GeglNode *grainmerge;
+  GeglNode *colordodge;
+  GeglNode *nop;
+  GeglNode *nop2;
+  GeglNode *outline;
+  GeglNode *coloroverlay;
+  GeglNode *imagefileupload;
+  GeglNode *image;
+  GeglNode *idref;
+}State;
+
+
 static void attach (GeglOperation *operation)
 {
   GeglNode *gegl = operation->node;
-  GeglColor *rock_hidden_color = gegl_color_new ("#ffffff");
+  GeglProperties *o = GEGL_PROPERTIES (operation);
 
-      GeglNode *input    = gegl_node_get_input_proxy (gegl, "input");
-      GeglNode *output   = gegl_node_get_output_proxy (gegl, "output");
+  State *state = o->user_data = g_malloc0 (sizeof (State));
 
 
-      GeglNode *color    = gegl_node_new_child (gegl,
+ GeglColor *rock_hidden_color = gegl_color_new ("#ffffff");
+
+      state->input    = gegl_node_get_input_proxy (gegl, "input");
+      state->output   = gegl_node_get_output_proxy (gegl, "output");
+
+
+     state->color    = gegl_node_new_child (gegl,
                                   "operation", "gegl:color-overlay", "value", rock_hidden_color,
                                   NULL);
 
-      GeglNode *median    = gegl_node_new_child (gegl,
+      state->median    = gegl_node_new_child (gegl,
                                   "operation", "gegl:median-blur",  "abyss-policy", 0,
                                   NULL);
 
 
-      GeglNode *median2    = gegl_node_new_child (gegl,
+       state->median2    = gegl_node_new_child (gegl,
                                   "operation", "gegl:median-blur", "abyss-policy", 0,
                                   NULL);
 
 
-      GeglNode *gaussian    = gegl_node_new_child (gegl,
+       state->gaussian    = gegl_node_new_child (gegl,
                                   "operation", "gegl:gaussian-blur", "clip-extent", 0, "abyss-policy", 0,
                                   NULL);
 
-      GeglNode *noise    = gegl_node_new_child (gegl,
+       state->noise    = gegl_node_new_child (gegl,
                                   "operation", "gegl:noise-spread",
                                   NULL);
 
-      GeglNode *exposure    = gegl_node_new_child (gegl,
+       state->exposure    = gegl_node_new_child (gegl,
                                   "operation", "gegl:exposure",
                                   NULL);
 
-      GeglNode *normal    = gegl_node_new_child (gegl,
+       state->normal    = gegl_node_new_child (gegl,
                                   "operation", "gegl:over",
                                   NULL);
 #define mysyntax \
 " id=1 gimp:layer-mode layer-mode=hsv-value  opacity=1 aux=[  ref=1 noise-hsv holdness=7 hue-distance=0 saturation-distance=0 value-distance=0.90 ]   "\
 
 
-      GeglNode *graph    = gegl_node_new_child (gegl,
+      state->graph    = gegl_node_new_child (gegl,
                                   "operation", "gegl:gegl", "string", mysyntax, 
                                   NULL);
 
 
-      GeglNode *opacity    = gegl_node_new_child (gegl,
+      state->opacity    = gegl_node_new_child (gegl,
                                   "operation", "gegl:opacity",
                                   NULL);
 
 
 
 
-      GeglNode *edgesmooth    = gegl_node_new_child (gegl,
+      state->edgesmooth    = gegl_node_new_child (gegl,
                                   "operation", "lb:edgesmooth", "alpha-percentile2", 67.0, "gaus", 0.75,  "value", 2.2, "abyss-policy", 0, 
                                   NULL);
 
 
 
-      GeglNode *alpha    = gegl_node_new_child (gegl,
+      state->alpha    = gegl_node_new_child (gegl,
                                   "operation", "gimp:threshold-alpha",
                                   NULL);
 
-      GeglNode *shift   = gegl_node_new_child (gegl,
+      state->shift   = gegl_node_new_child (gegl,
                                   "operation", "gegl:shift",
                                   NULL);
 
-     GeglNode *emboss    = gegl_node_new_child (gegl,
+     state->emboss    = gegl_node_new_child (gegl,
                                   "operation", "gegl:emboss",
                                   NULL);
 
 
-     GeglNode *mcol    = gegl_node_new_child (gegl,
+     state->mcol    = gegl_node_new_child (gegl,
                                   "operation", "gegl:multiply",
                                   NULL);
 
-     GeglNode *image    = gegl_node_new_child (gegl,
+     state->image    = gegl_node_new_child (gegl,
                                   "operation", "gegl:multiply",
                                   NULL);
 
-     GeglNode *nop    = gegl_node_new_child (gegl,
+     state->nop    = gegl_node_new_child (gegl,
                                   "operation", "gegl:nop",
                                   NULL);
 
-     GeglNode *smooth    = gegl_node_new_child (gegl,
+     state->multiply    = gegl_node_new_child (gegl,
+                                  "operation", "gegl:multiply",
+                                  NULL);
+
+     state->hardlight    = gegl_node_new_child (gegl,
+                                  "operation", "gegl:hard-light",
+                                  NULL);
+
+     state->colordodge    = gegl_node_new_child (gegl,
+                                  "operation", "gegl:color-dodge",
+                                  NULL);
+
+     state->grainmerge    = gegl_node_new_child (gegl,
+                                  "operation", "gimp:layer-mode", "layer-mode", 47, "composite-mode", 0,
+                                  NULL);
+
+
+     state->smooth    = gegl_node_new_child (gegl,
                                   "operation", "gegl:mean-curvature-blur", "iterations", 1,
                                   NULL);
 
 
-     GeglNode *nop2    = gegl_node_new_child (gegl,
+     state->nop2    = gegl_node_new_child (gegl,
+                                  "operation", "gegl:nop",
+                                  NULL);
+
+     state->idref    = gegl_node_new_child (gegl,
                                   "operation", "gegl:nop",
                                   NULL);
 
 
-     GeglNode *imagefileupload    = gegl_node_new_child (gegl,
+     state->imagefileupload    = gegl_node_new_child (gegl,
                                   "operation", "port:load",
                                   NULL);
 
-     GeglNode *coloroverlay    = gegl_node_new_child (gegl,
+    state->coloroverlay    = gegl_node_new_child (gegl,
                                   "operation", "gegl:color-overlay",
                                   NULL);
 
-     GeglNode *outline    = gegl_node_new_child (gegl,
+      state->outline    = gegl_node_new_child (gegl,
                                   "operation", "gegl:dropshadow",
                                   NULL);
 
 
-  gegl_operation_meta_redirect (operation, "size", median, "radius");
-  gegl_operation_meta_redirect (operation, "shift", shift, "shift");
-  gegl_operation_meta_redirect (operation, "seed2", shift, "seed");
-  gegl_operation_meta_redirect (operation, "amountx", noise, "amount-x");
-  gegl_operation_meta_redirect (operation, "amounty", noise, "amount-y");
-  gegl_operation_meta_redirect (operation, "seed", noise, "seed");
-  gegl_operation_meta_redirect (operation, "size2", median2, "radius");
-  gegl_operation_meta_redirect (operation, "gaussian", gaussian, "std-dev-x");
-  gegl_operation_meta_redirect (operation, "gaussian", gaussian, "std-dev-y");
-  gegl_operation_meta_redirect (operation, "azimuth", emboss, "azimuth");
-  gegl_operation_meta_redirect (operation, "elevation", emboss, "elevation");
-  gegl_operation_meta_redirect (operation, "alpha-percentile", median, "alpha-percentile");
-  gegl_operation_meta_redirect (operation, "radius", outline, "radius");
-  gegl_operation_meta_redirect (operation, "opacity", outline, "opacity");
-  gegl_operation_meta_redirect (operation, "x", outline, "x");
-  gegl_operation_meta_redirect (operation, "y", outline, "y");
-  gegl_operation_meta_redirect (operation, "grow_radius", outline, "grow-radius");
-  gegl_operation_meta_redirect (operation, "color", outline, "color");
-  gegl_operation_meta_redirect (operation, "exposure", exposure, "exposure");
-  gegl_operation_meta_redirect (operation, "mvalue", coloroverlay, "value");
-  gegl_operation_meta_redirect (operation, "src", imagefileupload, "src");
-  gegl_operation_meta_redirect (operation, "grains", opacity, "value");
+  gegl_operation_meta_redirect (operation, "size", state->median, "radius");
+  gegl_operation_meta_redirect (operation, "shift", state->shift, "shift");
+  gegl_operation_meta_redirect (operation, "seed2", state->shift, "seed");
+  gegl_operation_meta_redirect (operation, "amountx", state->noise, "amount-x");
+  gegl_operation_meta_redirect (operation, "amounty", state->noise, "amount-y");
+  gegl_operation_meta_redirect (operation, "seed", state->noise, "seed");
+  gegl_operation_meta_redirect (operation, "size2", state->median2, "radius");
+  gegl_operation_meta_redirect (operation, "gaussian", state->gaussian, "std-dev-x");
+  gegl_operation_meta_redirect (operation, "gaussian", state->gaussian, "std-dev-y");
+  gegl_operation_meta_redirect (operation, "azimuth", state->emboss, "azimuth");
+  gegl_operation_meta_redirect (operation, "elevation", state->emboss, "elevation");
+  gegl_operation_meta_redirect (operation, "depth", state->emboss, "depth");
+  gegl_operation_meta_redirect (operation, "alpha-percentile", state->median, "alpha-percentile");
+  gegl_operation_meta_redirect (operation, "radius", state->outline, "radius");
+  gegl_operation_meta_redirect (operation, "opacity", state->outline, "opacity");
+  gegl_operation_meta_redirect (operation, "x", state->outline, "x");
+  gegl_operation_meta_redirect (operation, "y", state->outline, "y");
+  gegl_operation_meta_redirect (operation, "grow_radius", state->outline, "grow-radius");
+  gegl_operation_meta_redirect (operation, "color", state->outline, "color");
+  gegl_operation_meta_redirect (operation, "exposure", state->exposure, "exposure");
+  gegl_operation_meta_redirect (operation, "mvalue", state->coloroverlay, "value");
+  gegl_operation_meta_redirect (operation, "src", state->imagefileupload, "src");
+  gegl_operation_meta_redirect (operation, "grains", state->opacity, "value");
 
-  gegl_node_link_many (input, color, median, noise, gaussian, shift, median2, emboss, alpha, image, outline, nop, mcol,  nop2, normal, smooth,  exposure, edgesmooth, output, NULL);
-  gegl_node_connect (image, "aux", imagefileupload, "output");
-  gegl_node_connect (mcol, "aux", coloroverlay, "output");
-  gegl_node_connect (normal, "aux", opacity, "output");
-  gegl_node_link_many (nop2, graph, opacity, NULL);
-  gegl_node_link_many (nop, coloroverlay, NULL);
 
 }
+
+
+static void
+update_graph (GeglOperation *operation)
+{
+  GeglProperties *o = GEGL_PROPERTIES (operation);
+  State *state = o->user_data;
+  if (!state) return;
+
+switch (o->rockblend) {
+        break;
+    case multiply:
+  gegl_node_link_many (state->input, state->color, state->median, state->noise, state->gaussian, state->shift, state->median2, state->idref, state->multiply, state->alpha, state->image, state->outline, state->nop, state->mcol,  state->nop2, state->normal, state->smooth,  state->exposure, state->edgesmooth, state->output, NULL);
+  gegl_node_connect (state->image, "aux", state->imagefileupload, "output");
+  gegl_node_connect (state->mcol, "aux", state->coloroverlay, "output");
+  gegl_node_connect (state->normal, "aux", state->opacity, "output");
+  gegl_node_link_many (state->nop2, state->graph, state->opacity, NULL);
+  gegl_node_link_many (state->nop, state->coloroverlay, NULL);
+  gegl_node_connect (state->multiply, "aux", state->emboss, "output");
+  gegl_node_link_many (state->idref, state->emboss, NULL);
+        break;
+    case hardlight:
+  gegl_node_link_many (state->input, state->color, state->median, state->noise, state->gaussian, state->shift, state->median2, state->idref, state->hardlight, state->alpha, state->image, state->outline, state->nop, state->mcol,  state->nop2, state->normal, state->smooth,  state->exposure, state->edgesmooth, state->output, NULL);
+  gegl_node_connect (state->image, "aux", state->imagefileupload, "output");
+  gegl_node_connect (state->mcol, "aux", state->coloroverlay, "output");
+  gegl_node_connect (state->normal, "aux", state->opacity, "output");
+  gegl_node_link_many (state->nop2, state->graph, state->opacity, NULL);
+  gegl_node_link_many (state->nop, state->coloroverlay, NULL);
+  gegl_node_connect (state->hardlight, "aux", state->emboss, "output");
+  gegl_node_link_many (state->idref, state->emboss, NULL);
+        break;
+    }
+  }
+
+
 
 static void
 gegl_op_class_init (GeglOpClass *klass)
 {
   GeglOperationClass *operation_class;
-
+GeglOperationMetaClass *operation_meta_class = GEGL_OPERATION_META_CLASS (klass);
   operation_class = GEGL_OPERATION_CLASS (klass);
 
   operation_class->attach = attach;
+  operation_meta_class->update = update_graph;
 
   gegl_operation_class_set_keys (operation_class,
     "name",        "lb:rock-text",
